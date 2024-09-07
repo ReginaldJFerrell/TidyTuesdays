82081844b1296c4925f4efee5a8e53de41199f9b
Reginald Ferrell <reginaldjferrell@gmail.com>
2024-09-07 13:55:41 -0400

    Updates

--- Wealth and Income/Wealth and Income 2021.R
+++ Wealth and Income/Wealth and Income 2021.R
@@ -10,19 +10,14 @@ packages <- c("haven", "ggplot2", "gapminder", "tidyverse", "dplyr", "stringr",
               "tidytuesdayR")
 # invisible(lapply(packages, install.packages, character.only = TRUE))
 invisible(lapply(packages, library, character.only = TRUE))
-# Read in with tidytuesdayR package 
-# Install from CRAN via: install.packages("tidytuesdayR")
-# This loads the readme and all the datasets for the week of interest
 
-# Either ISO-8601 date or year/week works!
-
+#Import
 tuesdata <- tidytuesdayR::tt_load('2021-02-09')
 tuesdata <- tidytuesdayR::tt_load(2021, week = 7)
 
 lifetime_earn <- tuesdata$lifetime_earn
 
-# Or read in the data manually
-
+#Import data from tidy tuesdays repo
 lifetime_earn <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/lifetime_earn.csv')
 student_debt <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/student_debt.csv')
 retirement <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/retirement.csv')
@@ -34,10 +29,58 @@ income_aggregate <- readr::read_csv('https://raw.githubusercontent.com/rfordatas
 income_distribution <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/income_distribution.csv')
 income_mean <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/income_mean.csv')
 
+#############
+# Debt over time
+###########
+#Playing around with the area plots for the time series data
+plot_01 <- student_debt %>% group_by(year) %>% 
+  summarise(avg_debt = sum(loan_debt)) %>% 
+  ggplot(aes(x=year, y=avg_debt),fill = "black") + 
+  geom_area(size=.5, colour="black") +
+  theme_minimal()+
+  labs(title="US Student Debt", 
+       subtitle="Change over time by Race", 
+       x="Year",
+       y="Average Debt") +  
+  scale_y_continuous(limits=c(0, 35000), breaks=seq(0,35000,5000)) +
+  scale_x_continuous(limits=c(1986, 2020)) +
+  theme(plot.title = element_text(face="bold",hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5 ))
+plot_01
 
 
-time_studentdebt <- student_debt %>% group_by(year) %>% summarise(avg_debt = sum(loan_debt)) 
-race_studentdebt <- student_debt %>% group_by(year,race) %>% summarise(avg_debt = sum(loan_debt)) 
+plot_02 <- student_debt %>% group_by(year,race) %>% 
+  summarise(avg_debt = sum(loan_debt)) %>% 
+  ggplot(aes(x=year, y=avg_debt,fill = race)) + 
+  geom_area(size=.5, colour="black") +
+  theme_minimal() +
+  labs(title="Monthly Time Series", 
+       subtitle="Returns Percentage from Economics Dataset", 
+       caption="Source: Economics", 
+       y="Returns %") +  # title and caption
+  scale_x_date(labels = lbls, 
+               breaks = brks) +  # change to monthly ticks and labels
+  theme(axis.text.x = element_text(angle = 90, vjust=0.5),  # rotate x axis text
+        panel.grid.minor = element_blank())
+scale_fill_manual(values = c("psavert"="#00ba38", "uempmed"="#f8766d")) +  # line color
+  theme(panel.grid.minor = element_blank())  # turn off minor grid
+
+
+# Plot
+ggplot(data, aes(x=time, y=percentage, fill=group)) + 
+  geom_area(alpha=0.6 , size=1, colour="black")
+
+ggplot(time_studentdebt, aes(x=year)) + 
+  geom_line(aes(y=avg_debt)) + 
+  labs(title="Monthly Time Series", 
+       subtitle="Returns Percentage from Economics Dataset", 
+       caption="Source: Economics", 
+       y="Returns %") +  # title and caption
+  scale_x_date(labels = lbls, 
+               breaks = brks) +  # change to monthly ticks and labels
+  theme(axis.text.x = element_text(angle = 90, vjust=0.5),  # rotate x axis text
+        panel.grid.minor = element_blank())
+
 
 family_wealth <- race_wealth %>% filter(year=="2016") 
 black_family <- family_wealth %>% filter(race=="Black") 
@@ -47,13 +90,12 @@ hispanic_family <- family_wealth %>% filter(race=="Hispanic")
 
 gender <- lifetime_earn %>% group_by(gender) %>% mutate(total_by_gender = sum(lifetime_earn)) %>% 
   mutate(pct_by_gender = (lifetime_earn/total_by_gender) * 100) %>%
-  
-  # mutate(women_earnings = ifelse(gender=="Women",max(total_by_gender),0),
-  #         men_earnings = ifelse(gender=="Men",max(total_by_gender),0),
-  #       total_earnings = sum(total_by_gender),
          pct_by_gender = (total_earnings/total_by_gender) * 100) %>%
   mutate(sum = sum(women_earnings + men_earnings))
    
+
+
+
 
 
 
